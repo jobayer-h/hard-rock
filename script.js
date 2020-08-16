5d9d01ae271eb418a32d7762adddeb4452e4e46c
jobayer-h <jobayer.dev@gmail.com>
2020-08-17 03:37:39 +0600

    song cover added

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ function getLyrics(artist, title) {
             lyricesDisplay.innerHTML = `<h2 class="text-success mb-4">${artist} - ${title}</h2>
                                         <pre class="lyric text-white">${lyrics}</pre>`
         })
-        document.getElementById('search-result').innerHTML = '';
+        document.getElementById('search-result').innerHTML = ''; // close song suggestions staff
 }
 
 
 
 function searchResult() {
     const songName = document.getElementById('song-name-input').value;
-    document.getElementById('search-result').innerHTML = '';
+    document.getElementById('search-result').innerHTML = ''; // for remove previously searched songs
+    document.getElementById('single-lyrics').innerHTML = ''; // for close previously opened Lyrics
     fetch(`https://api.lyrics.ovh/suggest/${songName}`)
     .then(res => res.json())
     .then(apiData => {
@@ -24,10 +25,14 @@ function searchResult() {
             const title = song.title;
             const artist = song.artist.name;
             const type = song.type;
-
+            const img = song.artist.picture_medium;
+// result template           
             const result = document.getElementById('search-result');
             result.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
-                                    <div class="col-md-9">
+                                        <div class="col-md-2">
+                                        <img class="img-thumbnail" src="${img}" alt="Cover Picture">
+                                        </div>
+                                        <div class="col-md-7">
                                         <h3 class="lyrics-name">${title}</h3>
                                         <p class="author lead">${type} by <span>${artist}</span></p>
                                     </div>
