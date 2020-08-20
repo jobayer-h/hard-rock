4a437d131d656f7ffc36a768a827f4c371f4bb78
jobayer-h <jobayer.dev@gmail.com>
2020-08-20 19:51:12 +0600

    hard rock v-0.3

--- script.js
+++ script.js
@@ -16,37 +16,47 @@ function getLyrics(artist, title) {
         })
         document.getElementById('search-result').innerHTML = ''; // close song suggestions staff
 }
-
+// enter button event handeler
+document.getElementById('song-name-input').addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        searchResult();
+    }
+  });
 
 
 function searchResult() {
     const songName = document.getElementById('song-name-input').value;
-    document.getElementById('search-result').innerHTML = ''; // for remove previously searched songs
-    document.getElementById('single-lyrics').innerHTML = ''; // for close previously opened Lyrics
-    fetch(`https://api.lyrics.ovh/suggest/${songName}`)
-    .then(res => res.json())
-    .then(apiData => {
-        const songs = apiData.data;
-        for (let i = 0; i < 10; i++) {
-            const song = songs[i];
-            const title = song.title;
-            const artist = song.artist.name;
-            const type = song.type;
-            const img = song.artist.picture_medium;
-// result template           
-            const result = document.getElementById('search-result');
-            result.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
-                                        <div class="col-md-2">
-                                        <img class="img-thumbnail" src="${img}" alt="Cover Picture">
+    if (songName.length == 0) {
+        alert('please enter your artist or Song')
+    } else {
+        document.getElementById('search-result').innerHTML = ''; // for remove previously searched songs
+        document.getElementById('single-lyrics').innerHTML = ''; // for close previously opened Lyrics
+        fetch(`https://api.lyrics.ovh/suggest/${songName}`)
+        .then(res => res.json())
+        .then(apiData => {
+            const songs = apiData.data;
+            for (let i = 0; i < 10; i++) {
+                const song = songs[i];
+                const title = song.title;
+                const artist = song.artist.name;
+                const type = song.type;
+                const img = song.artist.picture_medium;
+    // result template           
+                const result = document.getElementById('search-result');
+                result.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+                                            <div class="col-md-2">
+                                            <img class="img-thumbnail" src="${img}" alt="Cover Picture">
+                                            </div>
+                                            <div class="col-md-7">
+                                            <h3 class="lyrics-name">${title}</h3>
+                                            <p class="author lead">${type} by <span>${artist}</span></p>
                                         </div>
-                                        <div class="col-md-7">
-                                        <h3 class="lyrics-name">${title}</h3>
-                                        <p class="author lead">${type} by <span>${artist}</span></p>
-                                    </div>
-                                    <div class="col-md-3 text-md-right text-center">
-                                        <button onclick="getLyrics('${artist}','${title}')" class="btn btn-success">Get Lyrics</button>
-                                    </div>
-                                </div>`
+                                        <div class="col-md-3 text-md-right text-center">
+                                            <button onclick="getLyrics('${artist}','${title}')" class="btn btn-success">Get Lyrics</button>
+                                        </div>
+                                    </div>`
+            }
+        })
         }
-    })
 }
